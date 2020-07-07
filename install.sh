21d790e87199f82084cd14c0b4b11ea3b3c50491
Gutierri Barboza <me@gutierri.me>
2020-07-07 18:09:05 -0300

    update ratpoison/rofi/apt/X

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "installing packages..."
+# sudo apt install -y $(grep -iPo '"[a-z].+"' apt/Aptfile | tr -d '"')
+
+echo "stow links..."
+stow -R X bash git ratpoison screen vim zathura rofi sdorfehs
--- ratpoison/.config/ratpoison/scripts/startup.sh
+++ ratpoison/.config/ratpoison/scripts/startup.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+BASEDIR=$(dirname "$0")
+
 pkill dzen2
+pkill qconsole
 pkill stalonetray
+pkill redshift
+pkill xbanish
 
 sleep 0.5
 
-stalonetray &
+xsetroot -solid "#EBEBEB" &
+redshift &
+xrdb -load $HOME/.Xresources &
+stalonetray -c $HOME/.config/stalonetray/stalonetrayrc &
+qconsole -h 240 &
+xbanish &
+$BASEDIR/dzen_widgets.sh | dzen2 -ta l -w 1300 &
