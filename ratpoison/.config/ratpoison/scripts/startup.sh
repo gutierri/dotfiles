19c4dfc11e01c795256de5dc90bcd988d401bdc8
Gutierri Barboza <me@gutierri.me>
2020-07-10 15:10:50 -0300

    ratpoison: remove dzen2 and user bar hacks

--- ratpoison/.config/ratpoison/scripts/startup.sh
+++ ratpoison/.config/ratpoison/scripts/startup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(dirname "$0")
-
-pkill dzen2
 pkill qconsole
 pkill stalonetray
 pkill redshift
@@ -10,10 +7,7 @@ pkill xbanish
 
 sleep 0.5
 
-xsetroot -solid "#EBEBEB" &
 redshift &
-xrdb -load $HOME/.Xresources &
 stalonetray -c $HOME/.config/stalonetray/stalonetrayrc &
 qconsole -h 240 &
 xbanish &
-$BASEDIR/dzen_widgets.sh | dzen2 -ta l -w 1300 &
