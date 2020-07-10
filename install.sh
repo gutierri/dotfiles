d63a88e3093948b820738959cbb1c04bbeecd7d2
Gutierri Barboza <me@gutierri.me>
2020-07-10 15:11:39 -0300

    systemd with services from ratpoison startup

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "installing packages..."
 # sudo apt install -y $(grep -iPo '"[a-z].+"' apt/Aptfile | tr -d '"')
 
 echo "stow links..."
-stow -R X bash git ratpoison screen vim zathura rofi sdorfehs
+stow -R X bash git ratpoison screen vim zathura rofi systemd
